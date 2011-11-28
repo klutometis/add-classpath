3e85d996c5e8c1284cd03481f0feafd4516fab0e
Peter Danenberg <pcd@roxygen.org>
2011-11-28 05:02:39 -0800

    lein skeleton

---
+++ src/add_classpath/core.clj
@@ -0,0 +1,1 @@
+(ns add-classpath.core)
---
+++ test/add_classpath/test/core.clj
@@ -0,0 +1,6 @@
+(ns add-classpath.test.core
+  (:use [add-classpath.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
