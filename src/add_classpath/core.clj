39ab84e25129f8d08b7179418c46ea0bdc7d5787
Peter Danenberg <pcd@roxygen.org>
2011-11-28 09:16:26 -0800

    Check if file exists?

--- src/add_classpath/core.clj
+++ src/add_classpath/core.clj
@@ -1,6 +1,8 @@
 (ns add-classpath.core
-  (:import (java.net URLClassLoader URL)
-           (java.io File))
+  (:import (java.net URLClassLoader
+                     URL)
+           (java.io File
+                    FileNotFoundException))
   (:refer-clojure :exclude (add-classpath)))
 
 (let [add-url (.getDeclaredMethod URLClassLoader
@@ -8,6 +10,9 @@
                                   (into-array [URL]))]
   (.setAccessible add-url true)
   (defn add-classpath [file]
-    (.invoke add-url
-             (ClassLoader/getSystemClassLoader)
-             (into-array [(.toURL (.toURI (new File file)))]))))
+    (let [file (new File file)]
+      (if (.exists file)
+        (.invoke add-url
+                 (ClassLoader/getSystemClassLoader)
+                 (into-array [(.toURL (.toURI file))]))
+        (throw (new FileNotFoundException (.getPath file)))))))
