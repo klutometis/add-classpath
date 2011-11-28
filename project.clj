1f32655a4be883deaa5487bf070a4f1da05af599
Peter Danenberg <pcd@roxygen.org>
2011-11-28 08:06:06 -0800

    Untested

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject add-classpath "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "I want add-classpath back."
   :dependencies [[org.clojure/clojure "1.3.0"]])
--- src/add_classpath/core.clj
+++ src/add_classpath/core.clj
@@ -1,1 +1,13 @@
-(ns add-classpath.core)
+(ns add-classpath.core
+  (:import (java.net URLClassLoader URL)
+           (java.io File))
+  (:refer-clojure :exclude (add-classpath)))
+
+(let [add-url (.getDeclaredMethod URLClassLoader
+                                  "addURL"
+                                  (into-array [URL]))]
+  (.setAccessible add-url true)
+  (defn add-classpath [file]
+    (.invoke add-url
+             (ClassLoader/getSystemClassLoader)
+             (into-array [(.toURL (.toURI (new File file)))]))))
