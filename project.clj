6261ba8d934c806455738424147b3607f591214b
Peter Danenberg <pcd@roxygen.org>
2012-10-10 01:49:33 -0700

    1.0.3

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject add-classpath "1.0.3-SNAPSHOT"
+(defproject add-classpath "1.0.3"
   :description "I want add-classpath back."
   :url "https://github.com/klutometis/add-classpath"
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[debug "1.0.0-SNAPSHOT"]])
+  :dev-dependencies [[debug "1.0.1"]])
